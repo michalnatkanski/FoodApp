16f0fa98418d7be4967397270c8c04c0c74bb9c9
mnatkanski <m.natkanski0@gmail.com>
2022-10-16 20:48:04 +0200

    removed comments and components imports from app file

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 
 import * as React from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 //components
@@ -27,7 +23,3 @@ export default function App() {
   </NavigationContainer>
   );
 };
-
-//rozdzielic wszystko na komponenty
-//style do plikow
-//
